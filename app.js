69679a9a2f0f31b99863800ec0e14723fab12144
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-04-10 19:31:59 -0400

    added mongoDB

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import contactsRouter from './routes/contactsRouter.js';
-import dotenv from 'dotenv';
-import userRouter from './routes/auth.js';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import contactsRouter from "./routes/contactsRouter.js";
+import dotenv from "dotenv";
+import userRouter from "./routes/auth.js";
 
 dotenv.config();
+
 const app = express();
 
-mongoose
-  .connect(process.env.CONNECTION_STRING)
-  .then(() => {
-    console.log('Database connection successful');
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-  });
-
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
-app.use('/api/users', userRouter);
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", userRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running. Use our API on port: 3000');
-});
+const DB_HOST =
+  "mongodb+srv://GOIT_DataBase:UHKfPAMW647ppsrC@cluster0.vda3wk6.mongodb.net/my-users?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3030, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
