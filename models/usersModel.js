50611020f37d93cfa926ad7217e5e580028c3bda
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-04-20 11:04:01 -0400

    added chenges in controller avatar

--- models/usersModel.js
+++ models/usersModel.js
@@ -20,7 +20,7 @@ const userSchema = new Schema(
     gender: {
       type: String,
       enum: ["male", "female"],
-      default: null,
+      default: "",
     },
     token: {
       type: String,
